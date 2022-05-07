0dc552b20bd09103c9c4446d01e44269d7c49b3b
Sarwar Zahan <97876992+SZMithu@users.noreply.github.com>
2022-05-07 14:25:59 +0600

    Edit header section

--- js-file/script.js
+++ js-file/script.js
@@ -2,26 +2,31 @@
 
 $( document ).ready(function() {
   $(function(){
+    //Include header and footer
      $("#includedHeader").load("header.html"); 
      $("#includedFooter").load("footer.html"); 
   });
   //Sticy navbar
-let nav_offset_top = $('#includedHeader').height() + 50;
+let nav_offset_top = $('#includedHeader').height() + 300;
 function navbarFixed(){
 if($('#includedHeader').length){
   $(window).scroll(function(){
     let scroll = $(window).scrollTop();
     if(scroll >= nav_offset_top){
-      $('#includedHeader .header .navbar').addClass('fixed_nav');
+      $('#includedHeader .header').addClass('fixed_nav');
+       $('.logo').addClass('fixed');
+      $('.notice').css('margin-top', '420' + "px");
     }else{
-      $('#includedHeader .header .navbar').removeClass('fixed_nav');
+      $('#includedHeader .header').removeClass('fixed_nav');
+       $('.logo').removeClass('fixed');
+      $('.notice').css('margin-top', '0' + "px");
     }
   })
 }
 } 
 navbarFixed();
      
-$('.gallery .carousel .owl-carousel .client .client-img .popup-link').magnificPopup({
+$('.gallery .carousel .owl-carousel .client .client-img .popup-link, .service .container .popup-link').magnificPopup({
   type: 'image',
   gallery:{enabled:true}
 });
